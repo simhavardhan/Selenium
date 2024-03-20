8b6d9ba0e181cc068b27c5c783a9827e7a86eb35
simhavardhan <simhavardhan170@gmail.com>
2024-03-20 12:05:47 +0530

    random class is added

---
+++ src/ActionsClass/randomclass.java
@@ -0,0 +1,11 @@
+package ActionsClass;
+
+public class randomclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("random class added");
+
+	}
+
+}
